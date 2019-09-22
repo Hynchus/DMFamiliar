34fbfa3aa6e6724b8f54f1b90e3b792572cbfefa
Matthew Fraser <frasem@hotmail.ca>
2019-09-22 03:06:59 -0700

    When importing rules, rules are now separated into lists by type

--- RollTheDice/ImportRules.Designer.cs
+++ RollTheDice/ImportRules.Designer.cs
@@ -23,66 +23,127 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.RulesListbox = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.DamageRulesListbox = new System.Windows.Forms.ListBox();
+            this.Importbtn = new System.Windows.Forms.Button();
+            this.Cancelbtn = new System.Windows.Forms.Button();
+            this.EventRulesListbox = new System.Windows.Forms.ListBox();
+            this.DamageRuleslbl = new System.Windows.Forms.Label();
+            this.EventRuleslbl = new System.Windows.Forms.Label();
+            this.RulesListboxesPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.RulesListboxesPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // RulesListbox
+            // DamageRulesListbox
             // 
-            this.RulesListbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.DamageRulesListbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DamageRulesListbox.FormattingEnabled = true;
+            this.DamageRulesListbox.ItemHeight = 20;
+            this.DamageRulesListbox.Location = new System.Drawing.Point(3, 23);
+            this.DamageRulesListbox.Name = "DamageRulesListbox";
+            this.DamageRulesListbox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.DamageRulesListbox.Size = new System.Drawing.Size(201, 154);
+            this.DamageRulesListbox.TabIndex = 0;
+            // 
+            // Importbtn
+            // 
+            this.Importbtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Importbtn.Location = new System.Drawing.Point(126, 198);
+            this.Importbtn.Name = "Importbtn";
+            this.Importbtn.Size = new System.Drawing.Size(75, 34);
+            this.Importbtn.TabIndex = 1;
+            this.Importbtn.Text = "Import";
+            this.Importbtn.UseVisualStyleBackColor = true;
+            this.Importbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cancelbtn
+            // 
+            this.Cancelbtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Cancelbtn.Location = new System.Drawing.Point(236, 198);
+            this.Cancelbtn.Name = "Cancelbtn";
+            this.Cancelbtn.Size = new System.Drawing.Size(75, 34);
+            this.Cancelbtn.TabIndex = 2;
+            this.Cancelbtn.Text = "Cancel";
+            this.Cancelbtn.UseVisualStyleBackColor = true;
+            this.Cancelbtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EventRulesListbox
+            // 
+            this.EventRulesListbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EventRulesListbox.FormattingEnabled = true;
+            this.EventRulesListbox.ItemHeight = 20;
+            this.EventRulesListbox.Location = new System.Drawing.Point(210, 23);
+            this.EventRulesListbox.Name = "EventRulesListbox";
+            this.EventRulesListbox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.EventRulesListbox.Size = new System.Drawing.Size(201, 154);
+            this.EventRulesListbox.TabIndex = 3;
+            this.EventRulesListbox.SelectedIndexChanged += new System.EventHandler(this.EventRulesListbox_SelectedIndexChanged);
+            // 
+            // DamageRuleslbl
+            // 
+            this.DamageRuleslbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DamageRuleslbl.Location = new System.Drawing.Point(3, 0);
+            this.DamageRuleslbl.Name = "DamageRuleslbl";
+            this.DamageRuleslbl.Size = new System.Drawing.Size(201, 20);
+            this.DamageRuleslbl.TabIndex = 4;
+            this.DamageRuleslbl.Text = "Damage Rules";
+            this.DamageRuleslbl.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // EventRuleslbl
+            // 
+            this.EventRuleslbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EventRuleslbl.Location = new System.Drawing.Point(210, 0);
+            this.EventRuleslbl.Name = "EventRuleslbl";
+            this.EventRuleslbl.Size = new System.Drawing.Size(201, 20);
+            this.EventRuleslbl.TabIndex = 5;
+            this.EventRuleslbl.Text = "Event Rules";
+            this.EventRuleslbl.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // RulesListboxesPanel
+            // 
+            this.RulesListboxesPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.RulesListbox.FormattingEnabled = true;
-            this.RulesListbox.ItemHeight = 20;
-            this.RulesListbox.Location = new System.Drawing.Point(13, 13);
-            this.RulesListbox.Name = "RulesListbox";
-            this.RulesListbox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.RulesListbox.Size = new System.Drawing.Size(413, 184);
-            this.RulesListbox.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button1.Location = new System.Drawing.Point(126, 214);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Import";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button2.Location = new System.Drawing.Point(236, 214);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 34);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.RulesListboxesPanel.ColumnCount = 2;
+            this.RulesListboxesPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.RulesListboxesPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.RulesListboxesPanel.Controls.Add(this.DamageRulesListbox, 0, 1);
+            this.RulesListboxesPanel.Controls.Add(this.DamageRuleslbl, 0, 0);
+            this.RulesListboxesPanel.Controls.Add(this.EventRuleslbl, 1, 0);
+            this.RulesListboxesPanel.Controls.Add(this.EventRulesListbox, 1, 1);
+            this.RulesListboxesPanel.Location = new System.Drawing.Point(12, 12);
+            this.RulesListboxesPanel.Name = "RulesListboxesPanel";
+            this.RulesListboxesPanel.RowCount = 2;
+            this.RulesListboxesPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.RulesListboxesPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.RulesListboxesPanel.Size = new System.Drawing.Size(414, 180);
+            this.RulesListboxesPanel.TabIndex = 6;
             // 
             // ImportRules
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 260);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.RulesListbox);
+            this.ClientSize = new System.Drawing.Size(438, 244);
+            this.Controls.Add(this.RulesListboxesPanel);
+            this.Controls.Add(this.Cancelbtn);
+            this.Controls.Add(this.Importbtn);
+            this.MinimumSize = new System.Drawing.Size(460, 300);
             this.Name = "ImportRules";
             this.Text = "ImportRules";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ImportRules_FormClosing);
             this.Load += new System.EventHandler(this.ImportRules_Load);
+            this.RulesListboxesPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox RulesListbox;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox DamageRulesListbox;
+        private System.Windows.Forms.Button Importbtn;
+        private System.Windows.Forms.Button Cancelbtn;
+        private System.Windows.Forms.ListBox EventRulesListbox;
+        private System.Windows.Forms.Label DamageRuleslbl;
+        private System.Windows.Forms.Label EventRuleslbl;
+        private System.Windows.Forms.TableLayoutPanel RulesListboxesPanel;
     }
 }
